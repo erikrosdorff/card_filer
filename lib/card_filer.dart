188b6da6dd958e649c52bf8e65a0ac54e1609a83
Erik <erik.rosdorff@gmail.com>
2023-06-13 10:54:33 -0500

    I'm stuck

--- lib/card_filer.dart
+++ lib/card_filer.dart
@@ -1,7 +1,25 @@
 library card_filer;
 
+import 'package:empty_widget/empty_widget.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
+
+EmptyWidget(
+   image: null,
+   packageImage: PackageImage.Image_1,
+   title: 'No Notification',
+   subTitle: 'No  notification available yet',
+   titleTextStyle: TextStyle(
+     fontSize: 22,
+     color: Color(0xff9da9c7),
+     fontWeight: FontWeight.w500,
+   ),
+   subtitleTextStyle: TextStyle(
+     fontSize: 14,
+     color: Color(0xffabb8d6),
+   ),
+ );
